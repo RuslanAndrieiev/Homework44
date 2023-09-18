11ed4f0b9bd0b16f4728f7711f45a01bec00d9ae
Ruslan <ruslanklinvan@gmail.com>
2023-09-19 01:29:12 +0200

    Commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Дан List<Integer>. Написать метод, который определит,
+        // есть лм в этом списке такая пара чисел, которые в сумме дают заданное число N.
+        //1 8 12 5 сумма 6
+
+        List<Integer> num = new ArrayList<>();
+        num.add(1);
+        num.add(8);
+        num.add(12);
+        num.add(5);
+        System.out.println(num);
+
+
+    }
+}
